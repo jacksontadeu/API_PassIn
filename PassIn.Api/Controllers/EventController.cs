2d5ca0b9cf94d35c64e356010fe25b37a6ee7839
Jackson Moraes <jacksontadeu@hotmail.com>
2024-04-02 08:58:10 -0300

    :sparkles: feat: Inicio do projeto

---
+++ PassIn.Api/Controllers/EventController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Register;
+using PassIn.Communication.Requests;
+
+namespace PassIn.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EventController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestEventJson request)
+    {
+        try
+        {
+            var usecase = new RegisterEventUseCase();
+
+            usecase.Execute(request);
+
+            return Created();
+        }
+        catch (Exception ex) { }
+    }
+}
---
+++ PassIn.Application/UseCases/Register/RegisterEventUseCase.cs
@@ -0,0 +1,17 @@
+﻿using PassIn.Communication.Requests;
+
+namespace PassIn.Application.UseCases.Register;
+public class RegisterEventUseCase
+{
+    public void Execute(RequestEventJson request)
+    {
+        Validate(request);
+    }
+
+    private void Validate(RequestEventJson request)
+    {
+        if (request.MaximumAttendees <= 0) throw new ArgumentException("Número de participantes inválido");
+        if (string.IsNullOrWhiteSpace(request.Title)) throw new ArgumentException("O Título é invalido");
+        if (string.IsNullOrWhiteSpace(request.Details)) throw new ArgumentException("O detalhe é inválido");
+    }
+}
